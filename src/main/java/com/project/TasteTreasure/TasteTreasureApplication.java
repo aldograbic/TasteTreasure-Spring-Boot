9e779d7eb713e447d1e8d0662183ff9ba818f778
Goran Roso <fanatic.roso@gmail.com>
2023-10-23 20:11:52 +0200

    Prvi commit

---
+++ src/main/java/com/project/TasteTreasure/TasteTreasureApplication.java
@@ -0,0 +1,13 @@
+package com.project.TasteTreasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteTreasureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteTreasureApplication.class, args);
+	}
+
+}
