73c895a87a8d0944240667fdaa51fc640323865f
Aldo Grabić <aldo.grabic99@gmail.com>
2023-10-26 18:59:03 +0200

    search api radi barem nes

--- src/main/resources/static/js/fetchRecipes.js
+++ src/main/resources/static/js/fetchRecipes.js
@@ -1,12 +1,17 @@
 const APP_ID = "f995a27c";
-const APP_KEY = "e1979cdadca32155ca0b295a7449529d"; 
+const APP_KEY = "e1979cdadca32155ca0b295a7449529d";
 
-const API_URL = "https://api.edamam.com/api/recipes/v2";
+const API_URL = "https://api.edamam.com/search";
 
 async function fetchRecipes(query) {
   const response = await fetch(
     API_URL + "?q=" + query + "&app_id=" + APP_ID + "&app_key=" + APP_KEY
   );
-  const data = await response.json(); 
+  const data = await response.json();
   console.log(data);
+}
+
+async function searchRecipes() {
+  const query = document.getElementById("search").value;
+  await fetchRecipes(query);
 }
