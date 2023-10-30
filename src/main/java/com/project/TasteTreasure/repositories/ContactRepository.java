d7f9845bb0634d58804192625b09b650a0128040
Aldo Grabić <aldo.grabic99@gmail.com>
2023-10-30 16:41:50 +0100

    Napravljena kontakt funkcionalnost s obavijestima na index stranici

---
+++ src/main/java/com/project/TasteTreasure/repositories/ContactRepository.java
@@ -0,0 +1,8 @@
+package com.project.TasteTreasure.repositories;
+
+import com.project.TasteTreasure.classes.Contact;
+
+public interface ContactRepository {
+    
+    void saveContact(Contact contact);
+}
---
+++ src/main/java/com/project/TasteTreasure/repositories/JdbcContactRepository.java
@@ -0,0 +1,22 @@
+package com.project.TasteTreasure.repositories;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.project.TasteTreasure.classes.Contact;
+
+@Repository
+public class JdbcContactRepository implements ContactRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcContactRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void saveContact(Contact contact) {
+        String sql = "INSERT INTO messages(name, email, message) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, contact.getName(), contact.getEmail(), contact.getMessage());
+    }
+}
