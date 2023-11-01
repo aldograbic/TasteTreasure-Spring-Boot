13d3a3cfbfdc796755ebd7a169d3f94a2c207b68
Aldo Grabić <aldo.grabic99@gmail.com>
2023-11-01 10:16:10 +0100

    Dodana funkcionalnost za potvrdu emaila, dodane obavijesti na indexu i loginu, dodan API za prepoznavanje zastave korisnika po country code-u

--- src/main/java/com/project/TasteTreasure/config/DatabaseLoginFailureHandler.java
+++ src/main/java/com/project/TasteTreasure/config/DatabaseLoginFailureHandler.java
@@ -3,6 +3,7 @@ package com.project.TasteTreasure.config;
 import java.io.IOException;
 
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,8 @@ public class DatabaseLoginFailureHandler extends SimpleUrlAuthenticationFailureH
             response.sendRedirect("/login?error");
             // } else if (exception instanceof EmailNotVerifiedException) {
             // response.sendRedirect("/prijava?greskaPotvrda");
-            // } else if (exception instanceof InternalAuthenticationServiceException) {
-            // response.sendRedirect("/login?errorConfirm");
+            } else if (exception instanceof InternalAuthenticationServiceException) {
+            response.sendRedirect("/login?errorConfirm");
         } else {
             super.onAuthenticationFailure(request, response, exception);
         }
